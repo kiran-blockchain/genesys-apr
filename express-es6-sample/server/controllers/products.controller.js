4b391dda5c9d10823c6eeb6c063241b0bf620aff
BRUGUSOFT <kiran.paturi@brugu.io>
2022-04-06 17:45:17 +0530

    added code

--- express-es6-sample/server/controllers/products.controller.js
+++ express-es6-sample/server/controllers/products.controller.js
@@ -1,13 +1,36 @@
 
 
 import Product from '../models/product.model'
+import axios from 'axios'
 
 
-export const getProducts = (req,res)=>{
-    Product.find().then(result=>{
+export const getProducts = (req, res) => {
+    let callOne = axios.get('https://luckyblock.brugu.io/api/v1/getTotalMetrics')
+    let callTwo = Product.find();
+    Promise.all([callOne, callTwo]).then(result => {
+        res.json({
+            first: result[0].data.data,
+            second: result[1]
+        })
+
+    }).catch(ex => {
+        res.json(ex);
+    })
+    // .then(result=>{
+    //     res.json(result);
+
+    // }).catch(ex=>{
+    //     res.json(ex);
+    // })
+};
+
+export const addProduct = (req, res) => {
+    const product = req.body;
+    console.log(product);
+    const productToSave = new Product(product);
+    productToSave.save().then(result => {
         res.json(result);
-
-    }).catch(ex=>{
+    }).catch(ex => {
         res.json(ex);
     })
 }
--- express-es6-sample/server/routes/index.js
+++ express-es6-sample/server/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProducts } from '../controllers/products.controller';
+import { addProduct, getProducts } from '../controllers/products.controller';
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/products', getProducts);
+router.post("/addProduct",addProduct)
 
 export default router;
